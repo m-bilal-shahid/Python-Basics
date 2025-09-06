fd648896ada2a179dff79346ada2029a76191dbd
Muhammad Bilal Shahid <shkbilalshahid@gmail.com>
2025-09-06 21:36:56 +0500

    Add files via upload

---
+++ 01_Introduction.py
@@ -0,0 +1,58 @@
+
+# Python Basics - Topic 1: Introduction
+
+
+
+# 1.1 History of Python:
+
+# Python was created by Guido Van Rossum in 1991.
+# The old name of Python was "Monty Python's Flying Circus".
+# Old major version: Python 2 (supported until 2020)
+# Latest version: Python 3
+# To check your Python version check cmd or open vs code terminal and type 
+# python --version
+# ----------------------------
+
+# 1.2 Hello World in Python
+
+# The print() function is a built-in function in Python used to display output.
+
+print("Hello World")       # Single-line output
+print('Kamran')            # Single-line output with single quotes
+
+# Handling quotes inside strings
+print("Python's")          # Using double quotes to include single quote
+print('M "Kamran"')        # Using single quotes to include double quotes
+
+# Multi-line output using triple quotes
+print("""(1) Python Basics
+(2) Data Analysis
+(3) Data Science""")
+
+print('''(1) Python Basics
+(2) Data Analysis
+(3) Data Science''')
+
+# ----------------------------
+# 1.3 Comments in Python
+
+# Comments are non-executable lines in the code that are ignored by the Python interpreter.
+
+# They are used to explain the code, make it readable, and help others (or yourself) understand the logic.
+
+# Comments do not affect the program execution.
+# ----------------------------
+# Single-line comments start with hash (#) symbol
+# Example:
+# This is a comment for Python section 1
+
+# Multi-line comments can be written using triple quotes (''' or """)
+"""
+This is a multi-line comment.
+You can write multiple lines of explanation here.
+Useful for describing code sections in detail.
+"""
+'''
+Another example of a multi-line comment.
+You can write notes or documentation inside these quotes.
+'''

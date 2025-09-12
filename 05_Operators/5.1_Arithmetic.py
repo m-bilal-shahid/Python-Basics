7184650be92e2d520e8cb028b6fb84dee680065f
Muhammad Bilal Shahid <shkbilalshahid@gmail.com>
2025-09-12 23:21:56 +0500

    Created 5.1_Arithmetic.py

---
+++ 05_Operators/5.1_Arithmetic.py
@@ -0,0 +1,59 @@
+# Arithmetic Operators Are The Operators Used For Mathematical Calculations On Numbers
+
+
+# Arithmetic Operators in Python:
+
+#1. + (Addition):
+# # It Adds two or more numbers.
+# print(5 + 3)   
+
+
+# #2. - (Subtraction):
+# #  Subtracts the right number from the left.
+
+# print(5 - 3)   
+
+
+# #3.  * (Multiplication):
+# # Multiplies two numbers.
+
+# print(5 * 3)   
+
+
+# #4.  / (Division):
+# #  Divides left number by right → result is always float.
+
+# print(5 / 2)   
+
+
+# #5.  // (Floor Division):
+# #  Divides and gives the integer part (removes decimal).
+
+# print(5 // 2)  
+
+
+# #6. % (Modulus):
+# #  Returns the remainder of division.
+
+# print(5 % 2)   
+
+
+# #7. ** (Exponent / Power):
+# #  Raises a number to the power of another.
+
+# print(2 ** 3) 
+
+
+
+# Example:
+
+a=int(input("Enter First Number: "))
+b=int(input("Enter Second Number: "))
+
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+print("Floor Division:", a//b)
+print("Modulus:", a%b)
+print("Exponent:", a**b)
